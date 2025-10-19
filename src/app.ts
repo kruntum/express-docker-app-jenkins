5b941308731328233d745a55ea34fbae6ec7155a
TUMMY <kruntum@hotmail.com>
2025-10-19 20:31:52 +0700

    Test Jenkins CI/CD 2

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,8 @@ app.get('/api/products', (_: Request, res: Response) => {
     { id: 1, name: 'Product A', price: 100 },
     { id: 2, name: 'Product B', price: 150 },
     { id: 3, name: 'Product C', price: 200 },
-    { id: 4, name: 'Product D', price: 250 }
+    { id: 4, name: 'Product D', price: 250 },
+     { id: 5, name: 'Product D', price: 250 }
   ]
   res.json(products)
 })
